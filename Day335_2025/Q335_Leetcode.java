89782c415131ab615525077845c93a6fa7eb1cc4
Ayush Patel <155339843+ayushpatel497@users.noreply.github.com>
2025-12-01 08:04:15 +0530

    Add files via upload

---
+++ Day335_2025/Q335_Leetcode.java
@@ -0,0 +1,25 @@
+class Q335_Leetcode {
+    public long maxRunTime(int n, int[] batteries) {
+
+        long sumPower = 0;
+        for (long power : batteries)
+            sumPower += power;
+
+        long left = 1, right = sumPower / n;
+
+        while (left < right) {
+            long target = right - (right - left) / 2;
+            long extra = 0;
+
+            for (long power : batteries)
+                extra += Math.min(power, target);
+
+            if (extra >= (long)(n * target))
+                left = target;
+            else
+                right = target - 1;
+        }
+
+        return left;
+    }
+}
---
+++ Day335_2025/Q335_Leetcode.py
@@ -0,0 +1,22 @@
+from typing import List
+
+
+class Solution:
+    def maxRunTime(self, n: int, batteries: List[int]) -> int:
+
+        sumPower = sum(batteries)
+        left, right = 1, sumPower // n
+
+        while left < right:
+            target = right - (right - left) // 2
+            extra = 0
+
+            for power in batteries:
+                extra += min(power, target)
+
+            if extra >= n * target:
+                left = target
+            else:
+                right = target - 1
+
+        return left
