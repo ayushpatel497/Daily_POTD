952b59ac5eae00e35d29c640f065f864da717636
Ayush Patel <155339843+ayushpatel497@users.noreply.github.com>
2026-02-01 12:05:14 +0530

    Add files via upload

---
+++ Day032_2026/Q032_Leetcode.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int minimumCost(vector<int>& nums) {
+        // Q. Divide an Array Into Subarrays With Minimum Cost I
+        sort(nums.begin() + 1, nums.end());
+        return nums[0] + nums[1] + nums[2];
+    }
+};
