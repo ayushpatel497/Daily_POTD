e65b146e9e456bb66019fb1a38bbb14ed4be8ae8
Ayush Patel <155339843+ayushpatel497@users.noreply.github.com>
2026-02-03 21:48:22 +0530

    Add files via upload

---
+++ Day034_2026/Q034_GFG.cpp
@@ -0,0 +1,14 @@
+class Solution {
+  public:
+    int maxProfit(vector<int> &prices) {
+        // Q. Stock Buy and Sell – Max one Transaction Allowed
+        // code here
+        int result = INT_MIN;
+        int key = INT_MAX;
+        for(int i = 0; i < prices.size(); i++){
+            key = min(key, prices[i]);
+            result = max(result, prices[i] - key);
+        }
+        return result >= 0 ? result : 0;
+    }
+};
