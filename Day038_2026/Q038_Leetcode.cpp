9f1a6c1bdc3d05adb26aa9b54be95f5e53a80ad9
Ayush Patel <155339843+ayushpatel497@users.noreply.github.com>
2026-02-07 23:01:05 +0530

    Add files via upload

---
+++ Day038_2026/Q038_Leetcode.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minimumDeletions(string s) {
+        // Q. Minimum Deletions to Make String Balanced
+        int n = s.length();
+        int min_deletions = 0;
+        int b_count = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (s[i] == 'b') {
+                b_count++;
+            } else {
+                min_deletions = min(min_deletions + 1, b_count);
+            }
+        }
+
+        return min_deletions;
+    }
+};
