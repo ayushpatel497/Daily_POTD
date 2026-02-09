c4e80510a048ba8f986c2aa9adf8fb9f246e56cb
Ayush Patel <155339843+ayushpatel497@users.noreply.github.com>
2026-02-09 08:57:17 +0530

    Add files via upload

---
+++ Day040_2026/Q040_GFG.cpp
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+    int findKRotation(vector<int> &arr) {
+        // Q. Find Kth Rotation
+        // Code Here
+        int n = arr.size();
+        for(int i = 0; i < n-1; i++) {
+            if(arr[i] > arr[i+1]) return i+1;
+        }
+        return 0; 
+    }
+};
