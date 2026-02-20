4d1033fb7985f70f813c33d790de7d9d40fc79de
Ayush Patel <155339843+ayushpatel497@users.noreply.github.com>
2026-02-20 23:18:09 +0530

    Add files via upload

---
+++ Day051_2026/Q051_GFG.cpp
@@ -0,0 +1,23 @@
+class Solution {
+  public:
+    string findLargest(vector<int> &arr) {
+        // Q. Form the Largest Number
+        // code here
+        vector<string> temp;
+        for(int& num : arr){
+            temp.push_back(to_string(num));
+        }
+        
+        string res = "";
+        
+        sort(temp.begin(),temp.end(),[](string& str1,string& str2){
+            return str1 + str2 > str2 + str1;
+        });
+        
+        for(string& str : temp){
+            res += str;
+        }
+        
+        return res[0] == '0' ? "0" : res;
+    }
+};
