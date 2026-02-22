7caa2d1dedc134cddad329d2d07154f59eeeec28
Ayush Patel <155339843+ayushpatel497@users.noreply.github.com>
2026-02-22 10:05:54 +0530

    Add files via upload

---
+++ Day053_2026/Q053_GFG.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    long subarrayXor(vector<int> &arr, int k) {
+        // Q. Count Subarrays with given XOR
+        // code here
+        unordered_map<long, long> um;
+        um[0] = 1;
+        long currXOR = 0;
+        long ans = 0;
+        for(int i = 0; i < arr.size(); ++i){
+            currXOR ^= arr[i];
+            if(um.count(currXOR ^ k)){
+                ans += um[currXOR ^ k];
+            }
+            um[currXOR]++;
+        }
+        return ans;
+    }
+};
---
+++ Day053_2026/Q053_Leetcode.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int binaryGap(int n) {
+        // Q. Binary Gap
+        int last = -1, ans = 0;
+        for (int i = 0; i < 32; ++i)
+            if (((n >> i) & 1) > 0) {
+                if (last >= 0)
+                    ans = max(ans, i - last);
+                last = i;
+            }
+
+        return ans;
+    }
+};
