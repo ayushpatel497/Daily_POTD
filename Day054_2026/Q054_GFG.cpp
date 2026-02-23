1ad1566ed2bdd4d287d868778bba8c3ed9159018
Ayush Patel <155339843+ayushpatel497@users.noreply.github.com>
2026-02-23 23:40:39 +0530

    Add files via upload

---
+++ Day054_2026/Q054_GFG.cpp
@@ -0,0 +1,25 @@
+class Solution {
+  public:
+    vector<int> findUnion(vector<int>& a, vector<int>& b) {
+        // Q. Union of Arrays with Duplicates
+        // code here
+        unordered_map<int, int> m;
+        vector<int> ans;
+        
+        for(int i = 0; i < a.size(); i++){
+            if(m.count(a[i]) == 0){
+                m[a[i]]++;
+                ans.push_back(a[i]);
+            }
+        }
+        
+        for(int i = 0; i < b.size(); i++){
+            if(m.count(b[i]) == 0){
+                m[b[i]]++;
+                ans.push_back(b[i]);
+            }
+        }
+        
+        return ans;
+    }
+};
