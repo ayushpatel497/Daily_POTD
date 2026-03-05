adccbb668b1b7e292258471a5670a8913251a749
Ayush Patel <155339843+ayushpatel497@users.noreply.github.com>
2026-03-05 19:10:48 +0530

    Add files via upload

---
+++ Day064_2026/Q064_Leetcode.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minOperations(string s) {
+        // Q. Minimum Changes To Make Alternating Binary String
+        int start0 = 0;
+        
+        for (int i = 0; i < s.size(); i++) {
+            if (i % 2 == 0) {
+                if (s[i] == '1') {
+                    start0++;
+                }
+            } else {
+                if (s[i] == '0') {
+                    start0++;
+                }
+            }
+        }
+        
+        return min(start0, (int) s.size() - start0);
+    }
+};
