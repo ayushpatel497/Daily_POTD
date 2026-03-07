a1b21afa0ce0958998ee1ee61507b9a96bee9a83
Ayush Patel <155339843+ayushpatel497@users.noreply.github.com>
2026-03-07 13:02:23 +0530

    Add files via upload

---
+++ Day066_2026/Q066_GFG.cpp
@@ -0,0 +1,24 @@
+class Solution {
+    int solve(int faces, int dice, int remSum, vector<vector<int>>& dp){
+        if(dice == 0){
+            if(remSum == 0){
+              return 1;
+            }
+            return 0;
+        }
+        if(remSum < 0) return 0;
+        if(dp[dice][remSum] != -1) return dp[dice][remSum];
+        int ways = 0;
+        for(int m = 1; m <= faces; m++){
+            ways += solve(faces, dice - 1, remSum - m, dp);
+        }
+        return dp[dice][remSum] = ways;
+    }
+  public:
+    int noOfWays(int m, int n, int x) {
+        // Q. Dice throw
+        // code here
+        vector<vector<int>> dp(n + 1, vector<int>(x + 1, -1));
+        return solve(m, n, x, dp);
+    }
+};
