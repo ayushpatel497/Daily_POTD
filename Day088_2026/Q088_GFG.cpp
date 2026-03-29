2d08affa9e6f050cd41fdabcb116708efb6882dc
Ayush Patel <155339843+ayushpatel497@users.noreply.github.com>
2026-03-29 10:06:25 +0530

    Add files via upload

---
+++ Day088_2026/Q088_GFG.cpp
@@ -0,0 +1,24 @@
+class Solution {
+  public:
+    int countPartitions(vector<int>& arr, int diff) {
+        // Q. Partitions with Given Difference
+        // Code here
+        int totalSum = 0;
+        for (int num : arr) {
+            totalSum += num;
+        }
+        if ((totalSum + diff) % 2 != 0) {
+            return 0;
+        }
+        int target = (totalSum + diff) / 2;
+        vector<int> dp(target + 1, 0);
+        dp[0] = 1;
+        for (int num : arr) {
+            for (int j = target; j >= num; j--) {
+                dp[j] += dp[j - num];
+            }
+        }
+        
+        return dp[target];
+    }
+};
