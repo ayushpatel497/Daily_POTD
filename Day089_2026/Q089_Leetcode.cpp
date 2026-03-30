f4d840ab4d7c7caeed8d6f481a6842f768f4dbf5
Ayush Patel <155339843+ayushpatel497@users.noreply.github.com>
2026-03-30 07:40:30 +0530

    Add files via upload

---
+++ Day089_2026/Q089_Leetcode.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool checkStrings(string s1, string s2) {
+        // Q. Check if Strings Can be Made Equal With Operations II
+        if (s1.length() != s2.length()) {
+            return false;
+        }
+
+        int counts[256] = {0};
+
+        for (int i = 0; i < s1.length(); i++) {
+            int offset = (i & 1) << 7;
+            counts[offset + s1[i]]++;
+            counts[offset + s2[i]]--;
+        }
+
+        for (int i = 0; i < 256; i++) {
+            if (counts[i] != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
