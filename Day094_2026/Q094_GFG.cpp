6799750af4c27cff74cf924652e300686b3907db
Ayush Patel <155339843+ayushpatel497@users.noreply.github.com>
2026-04-04 09:29:19 +0530

    Add files via upload

---
+++ Day094_2026/Q094_GFG.cpp
@@ -0,0 +1,19 @@
+class Solution {
+    void gen(vector<string>&vec , int n){
+        if(n == 1)return;
+        int h = vec.size();
+        for(int i = h-1; i >=0 ; i--){
+            vec.push_back('1' + vec[i]);
+            vec[i] = '0' + vec[i];
+        }
+        gen(vec , n-1);
+  }
+  public:
+    vector<string> graycode(int n) {
+        // Q. Gray Code
+        // code here
+        vector<string>res = {"0" , "1"};
+        gen(res , n);
+        return res;
+    }
+};
