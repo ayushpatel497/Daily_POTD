40044b937cdaccd24818820d31ebb6e4cef22e23
Ayush Patel <155339843+ayushpatel497@users.noreply.github.com>
2026-04-12 23:27:08 +0530

    Add files via upload

---
+++ Day102_2026/Q102_GFG.cpp
@@ -0,0 +1,17 @@
+class Solution {
+  public:
+    bool isToeplitz(vector<vector<int>>& mat) {
+        // Q. Toeplitz matrix
+        // code here
+        int n = mat.size();
+        int m = mat[0].size();
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(i != 0 && j != 0){
+                    if(mat[i][j] != mat[i - 1][j - 1]) return false; 
+                }
+            }
+        }
+        return true;
+    }
+};
