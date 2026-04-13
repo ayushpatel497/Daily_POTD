f9ed4066659a0c116b4f470326260a41d7de52d4
Ayush Patel <155339843+ayushpatel497@users.noreply.github.com>
2026-04-13 22:50:21 +0530

    Add files via upload

---
+++ Day103_2026/Q103_Leetcode.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int getMinDistance(vector<int>& nums, int target, int start) {
+        // Q. Minimum Distance to the Target Element
+        int res = nums.size();
+        for (int i = 0; i < nums.size(); ++i) {
+            if (nums[i] == target) {
+                res = min(res, abs(i - start));
+            }
+        }
+        return res;
+    }
+};
