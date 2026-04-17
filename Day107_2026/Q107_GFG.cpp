c4211ed276d49d11f06acaa5a06a58937fa63750
Ayush Patel <155339843+ayushpatel497@users.noreply.github.com>
2026-04-17 08:52:11 +0530

    Add files via upload

---
+++ Day107_2026/Q107_GFG.cpp
@@ -0,0 +1,23 @@
+class Solution {
+  public:
+    bool canFormPalindrome(string &s) {
+        // Q. Anagram Palindrome
+        // code here
+        int freq[26] = {0};
+        for (char ch : s) {
+            freq[ch - 'a']++;
+        }
+        
+        int oddCount = 0;
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] % 2 != 0) {
+                oddCount++;
+            }
+            if (oddCount > 1) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
