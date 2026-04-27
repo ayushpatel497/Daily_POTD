59059690b45b480937829ec02ddea9804e7506b1
Ayush Patel <155339843+ayushpatel497@users.noreply.github.com>
2026-04-27 22:06:39 +0530

    Add files via upload

---
+++ Day117_2026/Q117_GFG.cpp
@@ -0,0 +1,25 @@
+class Solution {
+  public:
+    int smallestSubstring(string s) {
+        // Q. Smallest window containing 0, 1 and 2
+        // code here
+        int n = s.size();
+        int one = 0;
+        int zero = 0, two = 0;
+        int l = 0, r = 0, mini = INT_MAX;
+        while(r < n){
+            if(s[r] == '0') zero++;
+            else if(s[r] == '1') one++;
+            else if(s[r] == '2') two++;
+            while(zero >= 1 && one >= 1 && two >= 1){ 
+                mini = min(mini, (r - l + 1));
+               if(s[l] == '0') zero--;
+               else if(s[l] == '1') one--;
+               else if(s[l] == '2') two--;
+               l++;
+            }
+            r++;
+        }
+        return (mini == INT_MAX) ? -1 : mini;
+    }
+};
