b83c055e95437e26eea8e64356ee10e7790f6f61
Ayush Patel <155339843+ayushpatel497@users.noreply.github.com>
2026-05-07 09:35:29 +0530

    Add files via upload

---
+++ Day127_2026/Q127_GFG.cpp
@@ -0,0 +1,38 @@
+/*
+Definition for Node
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int x){
+        data = x;
+        left = right = nullptr;
+    }
+};
+*/
+
+class Solution {
+    bool isSame(Node* a, Node* b) {
+        if(a == NULL && b == NULL)
+            return true;
+        if(a == NULL || b == NULL)
+            return false;
+        if(a-> data != b-> data)
+            return false;
+        return isSame(a-> left, b-> left) && isSame(a-> right, b-> right);
+    }
+  public:
+    bool isSubTree(Node *root1, Node *root2) {
+        // Q. Check if subtree
+        // code here
+        if(root2 == NULL)
+            return true;
+        if(root1 == NULL)
+            return false;
+        if(isSame(root1, root2))
+            return true;
+        return isSubTree(root1-> left, root2) || isSubTree(root1-> right, root2);
+    }
+};
