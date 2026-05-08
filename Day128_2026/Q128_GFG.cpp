fa44455828005ef08843367dd317587f4d018b6f
Ayush Patel <155339843+ayushpatel497@users.noreply.github.com>
2026-05-08 21:11:07 +0530

    Add files via upload

---
+++ Day128_2026/Q128_GFG.cpp
@@ -0,0 +1,53 @@
+class Solution {
+    bool check(string &t) {
+        int bal = 0;
+        for(char c : t) {
+            if(c == '('){
+                bal++;
+            }
+            else if(c == ')'){
+                bal--;
+                if(bal < 0){
+                    return false;
+                }
+            }
+        }
+        return bal == 0;
+    }
+  public:
+    vector<string> validParenthesis(string &s) {
+        // Q. Remove Invalid Parentheses
+        // code here
+        unordered_set<string> vis;
+        queue<string> q;
+        vector<string> ans;
+        q.push(s);
+        vis.insert(s);
+        bool found = false;
+        while(!q.empty()){
+            string cur = q.front();
+            q.pop();
+            if(check(cur)) {
+                ans.push_back(cur);
+                found = true;
+            }
+            if(found){
+                continue;
+            }
+            for(int i = 0; i < cur.size(); i++) {
+                if(cur[i] != '(' && cur[i] != ')'){
+                    continue;
+                }
+                string nxt = cur.substr(0, i) + cur.substr(i + 1);
+                if(!vis.count(nxt)) {
+                    vis.insert(nxt);
+                    q.push(nxt);
+                }
+            }
+        }
+        sort(ans.begin(), ans.end());
+        ans.erase(unique(ans.begin(), ans.end()), ans.end());
+        
+        return ans;
+    }
+};
