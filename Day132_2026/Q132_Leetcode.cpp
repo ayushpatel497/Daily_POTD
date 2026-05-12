40be79e1d9dbeb44ea0050fc6c5af7631ee626d9
Ayush Patel <155339843+ayushpatel497@users.noreply.github.com>
2026-05-12 08:37:24 +0530

    Add files via upload

---
+++ Day132_2026/Q132_Leetcode.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minimumEffort(vector<vector<int>>& tasks) {
+        // Q. Minimum Initial Energy to Finish Tasks
+        sort(tasks.begin(), tasks.end(), [&](vector<int>& a, vector<int>& b) {
+            return a[1] - a[0] > b[1] - b[0];
+        });
+        int ans = 0;
+        int remain = 0;
+        for (auto task : tasks) {
+            ans += remain > task[1] ? 0 : task[1] - remain;
+            remain = max(task[1] - task[0], remain - task[0]);
+        }
+        return ans;
+    }
+};
