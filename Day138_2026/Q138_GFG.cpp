0e5314b446c200388dd2bf588376ed1d61f95a78
Ayush Patel <155339843+ayushpatel497@users.noreply.github.com>
2026-05-18 08:18:01 +0530

    Add files via upload

---
+++ Day138_2026/Q138_GFG.cpp
@@ -0,0 +1,9 @@
+class Solution {
+  public:
+    int maxSum(int n) {
+        // Q. Maximum Sum Problem
+        // code here.
+        if(n <= 1)  return n;
+        return max(n , maxSum(n/2) + maxSum(n/3) + maxSum(n/4));
+    }
+};
