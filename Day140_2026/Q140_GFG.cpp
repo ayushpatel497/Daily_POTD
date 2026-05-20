0108160e549a948228880fafdb70b7bbcdd9a1f0
Ayush Patel <155339843+ayushpatel497@users.noreply.github.com>
2026-05-20 16:26:40 +0530

    Add files via upload

---
+++ Day140_2026/Q140_GFG.cpp
@@ -0,0 +1,24 @@
+class Solution {
+  public:
+    bool isProduct(vector<int>& arr, long long target) {
+        // Q. Product Pair
+        // code here
+        unordered_set<long long> seen;
+        for (long long num : arr) {
+            if (num == 0) {
+                if (target == 0 && !seen.empty())
+                    return true;
+                seen.insert(num);
+                continue;
+            }
+            if (target % num == 0) {
+                long long needed = target / num;
+                if (seen.count(needed))
+                    return true;
+            }
+            seen.insert(num);
+        }
+
+        return false;
+    }
+};
