f1e557812cb137a077eb024576d9937408592516
Ayush Patel <155339843+ayushpatel497@users.noreply.github.com>
2026-06-11 22:27:32 +0530

    Add files via upload

---
+++ Day162_2026/Q162_GFG.cpp
@@ -0,0 +1,21 @@
+class Solution {
+  public:
+    int findIndex(string &s) {
+        // Q. Equal Point in Brackets
+        // code here
+        int rightClosing = 0;
+        for(char c: s){
+            if(c == ')') rightClosing++;
+        }
+        if(rightClosing ==0 ) return 0;
+        if(rightClosing == s.size()) return s.size();
+        int leftOpen = 0;
+        for(int i=0; i<s.size(); i++){
+            char c = s[i];
+            if(leftOpen == rightClosing) return i;
+            if(c=='(') leftOpen++;
+            if(c == ')') rightClosing--;
+        }
+        return s.size();
+    }
+};
