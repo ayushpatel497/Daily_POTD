c6c43ec4f27d2213502ddcf605002e7ad5c8175f
Ayush Patel <155339843+ayushpatel497@users.noreply.github.com>
2026-06-16 10:06:39 +0530

    Add files via upload

---
+++ Day167_2026/Q167_Leetcode.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string processStr(string s) {
+        // Q. Process String with Special Operations I
+        string result = "";
+        for (auto it : s) {
+            if (it == '*') {
+                if (result.size()) {
+                    result.pop_back();
+                }
+            } else if (it == '#') {
+                result += result;
+            } else if (it == '%') {
+                result = string(result.rbegin(), result.rend());
+            } else {
+                result += it;
+            }
+        }
+        return result;
+    }
+};
