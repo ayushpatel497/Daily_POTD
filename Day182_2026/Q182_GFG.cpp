b922b5c0a84e6166a21d7f3f65f2f0c025513681
Ayush Patel <155339843+ayushpatel497@users.noreply.github.com>
2026-07-01 22:14:01 +0530

    Add files via upload

---
+++ Day182_2026/Q182_GFG.cpp
@@ -0,0 +1,20 @@
+class Solution {
+  public:
+    int maxSumSubarray(vector<int>& arr) {
+        // Q. Max Subarray Sum by Removing At Most One
+        // code here
+        const int size = arr.size();
+        int ans = arr[0];
+        int prevNoDelete = arr[0];
+        int prevOneDelete = -1e9;
+        for(int i = 1; i < size; i++) {
+            int currNoDelete = max(prevNoDelete + arr[i], arr[i]);
+            int currOneDelete = max(prevNoDelete, prevOneDelete + arr[i]);
+            ans = max( {ans, currNoDelete, currOneDelete} );
+            prevNoDelete = currNoDelete;
+            prevOneDelete = currOneDelete;
+        }
+        
+        return ans;
+    }
+};
