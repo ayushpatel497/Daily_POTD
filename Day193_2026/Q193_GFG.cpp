c5f249daa170d1103ff6f94fb6f8bfffc1a6b612
Ayush Patel <155339843+ayushpatel497@users.noreply.github.com>
2026-07-12 10:27:34 +0530

    Add files via upload

---
+++ Day193_2026/Q193_GFG.cpp
@@ -0,0 +1,23 @@
+class Solution {
+  public:
+    int maxAmount(vector<int>& arr, int k) {
+        // Q. Max Amount by Selling K Tickets
+        // code here
+        const int MOD = 1000000007;
+        priority_queue<int> pq;
+        for (int x : arr)
+            if (x > 0)
+                pq.push(x);
+        long long ans = 0;
+        while (k-- && !pq.empty()) {
+            int cur = pq.top();
+            pq.pop();
+            ans = (ans + cur) % MOD;
+            cur--;
+            if (cur > 0)
+                pq.push(cur);
+        }
+
+        return (int)ans;
+    }
+};
