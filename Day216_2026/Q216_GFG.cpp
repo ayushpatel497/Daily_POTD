f0e0f82135df14bf2e06ce1bc1cb5e218ab849ef
Ayush Patel <155339843+ayushpatel497@users.noreply.github.com>
2026-08-04 23:29:45 +0530

    Add files via upload

---
+++ Day216_2026/Q216_GFG.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    int countPairs(vector<int>& arr, int k) {
+        // Q. Pairs with Less Than K Diff
+        // code here
+        int n = arr.size();
+        sort(arr.begin(), arr.end());
+        int i = 0, j = 0, cnt = 0;
+        for(; j < n; j++){
+            while(arr[j] - arr[i] >= k){
+                i++;
+            }
+            cnt += (j - i);
+        }
+        
+        return cnt;
+    }
+};
