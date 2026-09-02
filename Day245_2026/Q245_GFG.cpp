b584e04e043f01059d19f7f36aa613574d5a11e9
Ayush Patel <155339843+ayushpatel497@users.noreply.github.com>
2026-09-02 08:47:07 +0530

    Add files via upload

---
+++ Day245_2026/Q245_GFG.cpp
@@ -0,0 +1,28 @@
+class Solution {
+  public:
+    int solve(int n, string s) {
+        // Q. Unoccupied Computers
+        // code here
+        int state[26] = {0};
+        int available = n;
+        int rejected = 0;
+        for (char ch : s) {
+            int id = ch - 'A';
+            if (state[id] == 0) {
+                if (available > 0) {
+                    available--;
+                    state[id] = 1;
+                } else {
+                    rejected++;
+                    state[id] = 2;
+                }
+            } else {
+                if (state[id] == 1) {
+                    available++;
+                }
+            }
+        }
+
+        return rejected;
+    }
+};
---
+++ Day245_2026/Q245_Leetcode.cpp
@@ -0,0 +1,7 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        // Q. Construct Uniform Parity Array I
+        return true;
+    }
+};
