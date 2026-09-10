26d62b42931423e2d5db1fe622d48cee6c0ec92d
Ayush Patel <155339843+ayushpatel497@users.noreply.github.com>
2026-09-10 10:32:15 +0530

    Add files via upload

---
+++ Day253_2026/Q253_GFG.cpp
@@ -0,0 +1,30 @@
+class Solution {
+    int getGCD(int a , int b){
+        if(a == 0) return b;
+        return getGCD(b % a, a);
+    }
+  public:
+    int pairCount(int x, int y) {
+        // Q. Pairs with Given GCD and LCM
+        // code here
+        int left = 1;
+        int right = y;
+        int ans = 0;
+        while(left <= right){
+            if((left * right) < (x * y)){
+                left++;
+            }else if((left * right) > (x * y)){
+                right--;
+            }else{
+                int GCD = getGCD(left , right);
+                int LCM = (left * right) / GCD;
+                if(GCD == x && LCM == y){
+                    ans = ans + (left == right ? 1 : 2);
+                }
+                left++;
+                right--;
+            }
+        }
+        return ans;
+    }
+};
