42efeda75a71a73f85b856ffcd63d10bcf714151
Ayush Patel <155339843+ayushpatel497@users.noreply.github.com>
2026-09-15 12:03:42 +0530

    Add files via upload

---
+++ Day258_2026/Q258_GFG.cpp
@@ -0,0 +1,53 @@
+/* Binary Tree Node Structure
+class Node {
+  public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int x) {
+        data = x;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+*/
+
+class Solution {
+  public:
+    int getCount(Node *root, int k) {
+        // Q. Visit Leaves with Budget
+        // code here
+        queue<Node*> q;
+        q.push(root);
+        q.push(nullptr);
+        int n = 1;
+        int m = 0;
+        int cnt = 0;
+        while(!q.empty()){
+            Node* ans = q.front();
+            q.pop();
+            if(ans){
+                if(!ans-> left && !ans-> right){
+                    m = m + n;
+                    if(m <= k){
+                        cnt++;
+                    }
+                }
+                if(ans-> left){
+                    q.push(ans-> left);
+                }
+                if(ans-> right){
+                    q.push(ans-> right);
+                }
+            }
+            else{
+                n++;
+                if(!q.empty()){
+                   q.push(nullptr);
+                }
+            }
+        }
+        return cnt;
+    }
+};
