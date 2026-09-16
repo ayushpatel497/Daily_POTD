c19af3a835e71a9b9d801e580f0299d7e154a728
Ayush Patel <155339843+ayushpatel497@users.noreply.github.com>
2026-09-16 16:50:08 +0530

    Add files via upload

---
+++ Day259_2026/Q259_Leetcode.cpp
@@ -0,0 +1,23 @@
+class Solution {
+    const int MOD = 1000000007;
+    long long quickPow(long long a, long long e) {
+        long long result = 1;
+        while (e > 0) {
+            if (e & 1) result = result * a % MOD;
+            a = a * a % MOD;
+            e >>= 1;
+        }
+        return result;
+    }
+public:
+    int numberOfSets(int n, int k) {
+        // Q. Number of Sets of K Non-Overlapping Line Segments
+        int m = 2 * k;
+        long long numerator = 1, denominator = 1;
+        for (int i = 1; i <= m; i++) {
+            numerator = numerator * (n + k - i) % MOD;
+            denominator = denominator * i % MOD;
+        }
+        return numerator * quickPow(denominator, MOD - 2) % MOD;
+    }
+};
